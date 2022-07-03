87bb97aee069f50485a08051caff2b0faf1fc2a5
ECHOoooOh <1215202632@qq.com>
2022-07-03 12:59:38 +0000

    新增

---
+++ web/load_remote_file.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:html';
+
+final UListElement wordList = querySelector('#wordList') as UListElement;
+
+void main() {
+  querySelector('#getWords')!.onClick.listen(makeRequest);
+}
+
+Future<void> makeRequest(Event _) async {
+  const path = 'portmanteaux.json';
+  try {
+    final jsonString = await HttpRequest.getString(path);
+    processResponse(jsonString);
+  } catch (e) {
+    print('Could not open $path');
+    wordList.children.add(LIElement()..text = 'Request failed.');
+  }
+}
+
+void processResponse(String jsonString) {
+  for (final portmanteau in json.decode(jsonString) as List<dynamic>) {
+    wordList.children.add(LIElement()..text = portmanteau as String);
+  }
+}
